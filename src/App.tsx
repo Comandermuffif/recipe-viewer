90db4cf2c9c4542fb226f9a85b5df2ecdb1ca3fc
Vincent Coffey <vincent3coffey@gmail.com>
2024-09-08 12:54:56 +0000

    Move to uploaded recipe

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import Recipe from './Recipe';
-import data from '../recipes/lemon-garlic-shrimp.json';
+import { ChangeEvent, useState } from 'react';
+import RecipeData from './types';
 
 export default function App() {
+  const [recipeData, setRecipe] = useState<RecipeData | null>(null);
+
+  async function handleFileChange(event: ChangeEvent<HTMLInputElement>): Promise<void> {
+    if (event.target.files) {
+      const file = event.target.files[0];
+      setRecipe(JSON.parse(await file.text()));
+    }
+  }
+
   return (
-    <Recipe {...data}/>
+    <div>
+      <input id="file" type="file" onChange={handleFileChange} hidden={recipeData != undefined}/>
+      <Recipe {...recipeData}/>
+    </div>
   )
 };
